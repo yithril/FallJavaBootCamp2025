e43a4507482ea1bfe1953eb998752574f32f0dd2
yithril <yithril@gmail.com>
2025-10-02 09:05:37 -0400

    warmup update

---
+++ Week2-StringsAndArrays/3.Wednesday/Encapsulation/src/main/java/org/example/CellPhone.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class CellPhone {
+    private int serialNumber;
+    //properties
+    //constructors
+    //parameterless constructor
+    public CellPhone(){
+        this.serialNumber = 0;
+    }
+
+    //getters and setters
+}
