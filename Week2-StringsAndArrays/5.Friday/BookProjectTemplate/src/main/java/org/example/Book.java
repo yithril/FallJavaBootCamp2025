679a60a00b8eb66f4fc8269a2e628ead25236da8
yithril <yithril@gmail.com>
2025-10-03 01:08:50 -0400

    example help

---
+++ Week2-StringsAndArrays/5.Friday/BookProjectTemplate/src/main/java/org/example/Book.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//Here is an outline of what should go in your book class
+//There should NOT be any code that deals with menus
+//There should NOT be any code that takes in user input
+//All this class cares about is book data
+//and what to do when the book is checked in or out, that's it
+public class Book {
+    //properties
+
+    //constructor
+
+    //getters and setters
+
+    //check in and check out methods
+    public void checkIn(){
+
+    }
+
+    public void checkOut(){
+
+    }
+}
---
+++ Week2-StringsAndArrays/5.Friday/BookProjectTemplate/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+//This template is to help you know where certain code should live
+//This is to help you along if you're stuck
+public class Main {
+    public static void main(String[] args) {
+        //You'll need an array of 20 books, put it here
+
+        //You'll need a scanner to take in user input. Make one.
+
+        //You'll need a menu. A while(true) loop will be helpful
+
+        //Based on what they type, make a switch case to handle it
+        //Be sure they can exit the program!
+
+        //Use a for loop and sout to print all the book titles
+        //remember to use your Book class getters
+        //Use if statements to filter out books that are checkedin/out
+        //as need be
+
+        //You will call the checkIn() and checkOut() methods
+        //If the user wants to check in or out
+        //Remember if they check out you must ask for their name!
+        //example: book.checkOut(name) or book.checkIn()
+    }
+
+
+}
