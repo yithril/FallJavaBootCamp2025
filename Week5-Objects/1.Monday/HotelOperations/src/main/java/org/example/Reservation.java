600af3c6de36126e23507a9bb5885749c3dadf97
yithril <yithril@gmail.com>
2025-10-21 09:08:32 -0400

    hotel operations

--- Week5-Objects/1.Monday/HotelOperations/src/main/java/org/example/Reservation.java
+++ Week5-Objects/1.Monday/HotelOperations/src/main/java/org/example/Reservation.java
@@ -1,4 +1,55 @@
 package org.example;
 
 public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getPrice(){
+        if(roomType.equalsIgnoreCase("king")){
+            return 139;
+        }
+        else{
+            return 124;
+        }
+    }
+
+    public double getReservationTotal(){
+        if(isWeekend){
+            return getPrice() * 1.1 * numberOfNights;
+        }
+        else{
+            return getPrice() * numberOfNights;
+        }
+    }
 }
--- Week5-Objects/1.Monday/HotelOperations/src/main/java/org/example/Room.java
+++ Week5-Objects/1.Monday/HotelOperations/src/main/java/org/example/Room.java
@@ -1,4 +1,52 @@
 package org.example;
 
 public class Room {
+    private int numberOfBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+
+    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        isDirty = dirty;
+    }
+
+    //derived getter
+    public boolean isAvailable(){
+        return !isDirty && !isOccupied;
+    }
 }
